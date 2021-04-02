224dec07275e1987a6a310fc1bb48c753fb8c2ee
xandenunes <xandenunes302@outlook.com>
2021-04-02 20:59:20 -0300

    feat:projeto finalizado

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### Mentalista
 * desafio feito pela alura com o objetivo e treinar a relação entre o js e o html
-* Tentando fazer o usurario acertar o numero e mudar o fundo dando ate 3 tentativas pra ele por enquanto esta dando erro
+* O obejtico do projeto é o um jogo no qual o usuario tem que acertar o numero em 3 tentativas se ele acertar muda o fundo como comemoração se nao muda triste
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var acertou=0;
-var number = 3;
+var number =Math.floor(Math.random() * 10);
+console.log(number)
 var chances=3;
 function testa_numero() { 
     numberUser=(document.getElementById('numberUser').value)
@@ -9,16 +10,20 @@ function testa_numero() {
     else {
         chances--;
     }
-    if (acertou==1) {
-        document.body.style.backgroundImage = "url('mentalista.jpg')";
-        document.write(`<h2> Você acertou. Agora você é um mentalista</h2>`);
-    }if (chances<3 && chances>0) {
-        document.write(`<h2> Você errou. você tem mais ${chances} tentativas </h2>`);
+    if (chances<3 && chances>0) {
+        const y=document.getElementById( "resposta" );
+        y.innerHTML=`Você errou. você tem mais ${chances} tentativas`;
     }  
     else if(chances==0){
         document.body.style.backgroundImage = "url('trsie.jpg')";
-        document.write(`<h2> Suas tentativas acabaram </h2>`);
+        const z=document.getElementById( "resposta" );
+        z.innerHTML=`Suas tentativas acabaram`;
     } 
+    if (acertou==1) {
+        document.body.style.backgroundImage = "url('mentalista.jpg')";
+        const x=document.getElementById( "resposta" );
+        x.innerHTML=`Você acertou. Agora você é um mentalista`;
+    }
 }
 
 
